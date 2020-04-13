8b8d340b2fea454163dc11b0a3743aa16f88bc4e
jmend062 <jmend062@ucr.edu>
2020-04-12 17:33:19 -0700

    Finished Sprint 1

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+@app.route("/", methods = ['GET', 'POST'])
+def begin():
+    if request.method == 'POST':
+        Message = request.form['Message']
+        print("{} has been recieved".format(Message))
+        return redirect(url_for('home', Message=Message))
+    return render_template('Home.html')
+
+@app.route("/Home", methods = ['GET','POST'])
+def home():
+    Message = request.args.get('Message',None)
+    if request.method == 'POST':
+        return redirect(url_for('begin'))
+    return render_template('Recieved.html', Message=Message)
+        
+app.run(debug = True)
