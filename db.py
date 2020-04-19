9772ae576629ebddbfd2cd49a2efcc61c80320bb
miguelandrade4 <miguel.jr.andrade49@gmail.com>
2020-04-18 18:00:04 -0700

    Parser changed to be a function. Database now stores datagit status search helper function on the way.

--- db.py
+++ db.py
@@ -1,18 +1,27 @@
 class ytDB:
 	def __init__(self):
 		self.db = dict()
+		self.videoData = self.videoData()
+		self.counter = 1 #video ids are not unique so counter will be keys to dictionary
 
-	def ytDBStart(self, uID, columns):
-		if uID not in self.db:
-			self.db[uID] = columns
+	def ytDBStart(self, columns):
+		if len(columns) == 16:
+			self.videoData.setVars(columns[0], columns[1], columns[2], columns[3], columns[4],
+							columns[5], columns[6], columns[7], columns[8], columns[9],
+							columns[10],columns[11], columns[12], columns[13], columns[14],
+							columns[15])
+			self.db[self.counter] = self.videoData
+			self.counter += 1
+		else: 
+			return
 
-	def searchDB(self, uID):
-		if self.db is None or self.db[]:
-			return None
-		else:
-			for unique in self.db:
-				if unique is uID:
-					print(self.db[uID])
+	# def searchDB(self, uID):
+	# 	if self.db is None or self.db[]:
+	# 		return None
+	# 	else:
+	# 		for unique in self.db:
+	# 			if unique is uID:
+	# 				print(self.db[uID])
 
 
 	# def add():
@@ -21,3 +30,42 @@ class ytDB:
 
 	# def update():
 
+	#added inner class to make content retrieval and search easier
+	class videoData(object):
+		def __init__(self):
+			self.videoID = str()
+			self.trendingDate = str() 
+			self.Title = str()
+			self.channelTitle = str()
+			self.categoryID = str()
+			self.publishTime = str()
+			self.tags = str()
+			self.views = str()
+			self.likes = str()
+			self.dislikes = str()
+			self.commentCount = str()
+			self.thumbLink = str()
+			self.comDisabled = str()
+			self.ratingsDisabled = str()
+			self.videoEOR = str()
+			self.description = str()
+			
+		def setVars(self, videoID, trendingDate, Title, channelTitle, categoryID,
+					publishTime, tags, views, likes, dislikes, commentCount,
+					thumbLink, comDisabled, ratingsDisabled, videoEOR, description):
+			self.videoID = videoID
+			self.trendingDate = trendingDate 
+			self.Title = Title
+			self.channelTitle = channelTitle
+			self.categoryID = categoryID
+			self.publishTime = publishTime
+			self.tags = tags
+			self.views = views
+			self.likes = likes
+			self.dislikes = dislikes
+			self.commentCount = commentCount
+			self.thumbLink = thumbLink
+			self.comDisabled = comDisabled
+			self.ratingsDisabled = ratingsDisabled
+			self.videoEOR = videoEOR
+			self.description = description
--- parser.py
+++ parser.py
@@ -1,35 +1,34 @@
 import os
 import re
+from db import *
 
-filename = os.getcwd()
-prev_row = []
-temp = 0; skip_header = 0
+def parser(database):
+	filename = os.getcwd()
+	prev_row = []
+	temp = 0; skip_header = 0
 
-
-with open(os.path.join(filename,"CSVFile/USvideos.csv"), "r") as data:
-    for row in data:
-        if (skip_header == 0):
-            skip_header = 1
-        else:
-            count = 0
-            count = row.count('"')
-            
-            if (count % 2 == 0 and count != 0):
-                fields = re.split((",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"), row)
-                print(fields)
-                #CallDBClass(fields)
-            
-            else:  
-                if (temp == 0):
-                    temp = 1
-                    prev_row = row
-                
-                else:
-                    prev_row = prev_row + row
-                    if (count == 1):
-                        temp = 0
-                        prev_row = prev_row.replace("\r"," ")
-                        prev_row = prev_row.replace("\n"," ")
-                        fields = re.split((",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"), prev_row)
-                        print(fields)
-                        #CallDBClass(fields)
+	with open(os.path.join(filename,"CSVFile/USvideos.csv"), "r") as data:
+	    for row in data:
+	        if (skip_header == 0):
+	            skip_header = 1
+	        else:
+	            count = 0
+	            count = row.count('"')
+	            
+	            if (count % 2 == 0 and count != 0):
+	                fields = re.split((",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"), row)
+	                database.ytDBStart(fields)
+	            
+	            else:  
+	                if (temp == 0):
+	                    temp = 1
+	                    prev_row = row
+	                
+	                else:
+	                    prev_row = prev_row + row
+	                    if (count == 1):
+	                        temp = 0
+	                        prev_row = prev_row.replace("\r"," ")
+	                        prev_row = prev_row.replace("\n"," ")
+	                        fields = re.split((",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"), prev_row)
+	                        database.ytDBStart(fields)
